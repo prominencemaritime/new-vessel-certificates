7f671fa332bb18e90fea8556577c501dde64707b
Dimitri Skliros <skliros@quantsuite.io>
2025-11-24 10:31:22 +0000

    Minor tweak - trimmed sql

--- queries/NewVesselCertificates.sql
+++ queries/NewVesselCertificates.sql
@@ -9,10 +9,6 @@ SELECT
 	vd.expiration_date,
 	vd.comments
 FROM vessel_documents vd
-LEFT JOIN parties p1
-	on p1.id = vd.created_by_id
-LEFT JOIN parties p2
-	ON p2.id = vd.updated_by_id
 LEFT JOIN vessel_document_types vdt
 	ON vdt.id = vd.vessel_document_type_id
 LEFT JOIN vessels v
